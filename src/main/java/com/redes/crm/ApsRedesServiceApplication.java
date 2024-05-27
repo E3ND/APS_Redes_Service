c6aaa57770250293a7a731dd3a62a3c125cf9141
E3ND <brunopimentelcityhotmail.com@gmail.com>
2024-05-26 22:19:38 -0300

    fix env

--- src/main/java/com/redes/crm/ApsRedesServiceApplication.java
+++ src/main/java/com/redes/crm/ApsRedesServiceApplication.java
@@ -2,9 +2,6 @@ package com.redes.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class ApsRedesServiceApplication {

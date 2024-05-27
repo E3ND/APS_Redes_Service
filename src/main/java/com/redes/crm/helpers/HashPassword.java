e988d8be6b02ffb32f7030f0bc45ad1a5e4e3e8e
E3ND <brunopimentelcityhotmail.com@gmail.com>
2024-05-26 23:25:38 -0300

    teste

--- src/main/java/com/redes/crm/helpers/HashPassword.java
+++ src/main/java/com/redes/crm/helpers/HashPassword.java
@@ -1,23 +1,23 @@
-package com.redes.crm.helpers;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-@Component
-public class HashPassword {
-	 private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-	 
-	    public void PasswordEncoderService() {
-	        this.passwordEncoder = new BCryptPasswordEncoder();
-	    }
-
-	    public String encodePassword(String password) {
-	    	System.out.printf("Opa => ", password);
-	        return passwordEncoder.encode(password);
-	    }
-
-	    public boolean matchPassword(String rawPassword, String encodedPassword) {
-	        return passwordEncoder.matches(rawPassword, encodedPassword);
-	    }
-}
+//package com.redes.crm.helpers;
+//
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Service;
+//
+//@Component
+//public class HashPassword {
+//	 private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//	 
+//	    public void PasswordEncoderService() {
+//	        this.passwordEncoder = new BCryptPasswordEncoder();
+//	    }
+//
+//	    public String encodePassword(String password) {
+//	    	System.out.printf("Opa => ", password);
+//	        return passwordEncoder.encode(password);
+//	    }
+//
+//	    public boolean matchPassword(String rawPassword, String encodedPassword) {
+//	        return passwordEncoder.matches(rawPassword, encodedPassword);
+//	    }
+//}
